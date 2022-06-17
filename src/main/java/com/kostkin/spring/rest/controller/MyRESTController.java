fbc75a672f26b6245a2e42d61aaf029a26d896e1
Skaletto07 <Skaletto07@yandex.ru>
2022-06-17 21:18:53 +0300

    first step

---
+++ src/main/java/com/kostkin/spring/rest/controller/MyRESTController.java
@@ -0,0 +1,27 @@
+package com.kostkin.spring.rest.controller;
+
+
+import com.kostkin.spring.rest.entity.Employee;
+import com.kostkin.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRESTController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+
+}
